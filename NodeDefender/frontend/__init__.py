10552f80347f35226cb24399f12db0726dcc342f
Henrik Nilsson <henriknilsson.1991@gmail.com>
2016-12-20 16:01:31 +0100

    Updated login-view, now possible to login

--- NodeDefender/frontend/__init__.py
+++ NodeDefender/frontend/__init__.py
@@ -4,7 +4,6 @@ from flask import Blueprint
 from . import assets
 
 #Create Blueprint
-AuthView = Blueprint('AuthView', __name__, template_folder="templates/auth")
 AdminView = Blueprint('AdminView', __name__, template_folder="templates/admin",
                       static_folder="static")
 DataView = Blueprint('DataView', __name__, template_folder="templates/data",
@@ -35,7 +34,6 @@ def index():
     nodeevents = NodeEventModel.query.order_by(desc(NodeEventModel.id)).limit(20)
     return render_template('index.html', nodelist=nodes, stats = stats, nodeevents = nodeevents)
 
-from .auth import views
 from .admin import views
 from .data import views
 from .nodes import views
@@ -43,7 +41,6 @@ from .user import views
 
 # Register Blueprints
 app.register_blueprint(AdminView)
-app.register_blueprint(AuthView)
 app.register_blueprint(DataView)
 app.register_blueprint(NodeView)
 app.register_blueprint(UserView)
