a1508bec00434d0b059259062c8198baf1784f23
Henrik Nilsson <henriknilsson.1991@gmail.com>
2017-09-09 19:28:48 +0200

    refactoring frontend templates

--- NodeDefender/templates/frontend/static/js/sockets.js
+++ NodeDefender/templates/frontend/static/js/sockets.js
@@ -1,14 +1,12 @@
-var groupSocket = io.connect('//' + document.domain + ':' + location.port + '/group');  
-var nodeSocket = io.connect('//' + document.domain + ':' + location.port + '/node'); 
-var userSocket = io.connect('//' + document.domain + ':' + location.port + '/user'); 
+var groupSocket = io.connect('//' + document.domain + ':' + location.port + '/group');
+var nodeSocket = io.connect('//' + document.domain + ':' + location.port + '/node');
+var userSocket = io.connect('//' + document.domain + ':' + location.port + '/user');
 var iCPESocket = io.connect('//' + document.domain + ':' + location.port + '/icpe');
 var generalSocket = io.connect('//' + document.domain + ':' + location.port + '/general');
 var adminSocket = io.connect('//' + document.domain + ':' + location.port + '/admin');
 var dataSocket = io.connect('//' + document.domain + ':' + location.port + '/data');
 var plotlySocket = io.connect('//' + document.domain + ':' + location.port + '/plotly');
 var sensorSocket = io.connect('//' + document.domain + ':' + location.port + '/sensor');
-var mqttSocket = io.connect('//' + document.domain + ':' + location.port + '/mqtt');
-
 
 generalSocket.on('reload', function() {
 	console.log('Reloading..');
