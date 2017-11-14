ec211ee2acfd49b49ddbb894c0c2b16b52ceab86
Henrik Nilsson <henriknilsson.1991@gmail.com>
2017-11-14 21:17:39 +0100

    hotfixes in server- settings and adding user

--- NodeDefender/config/database.py
+++ NodeDefender/config/database.py
@@ -22,7 +22,7 @@ def db():
     return NodeDefender.config.parser['DATABASE']['DB']
 
 def file():
-    return NodeDefender.config.parser['DATABASE']['FILE_PATH']
+    return NodeDefender.config.parser['DATABASE']['FILEPATH']
 
 def mysql_uri():
     return 'mysql+pymysql://'+username()+':'+password()+'@'+server()+':'+port()+'/'+db()
