294bc2ab81ac509bc0304b9385e273eb81acf0c7
Henrik Nilsson <henriknilsson.1991@gmail.com>
2018-02-11 23:09:50 +0100

    structure of API and Deploy

--- NodeDefender/frontend/api/__init__.py
+++ NodeDefender/frontend/api/__init__.py
@@ -1,12 +1,17 @@
 import NodeDefender
-import NodeDefender.frontend.api.config
-from flask_restful import Api
+from flask import Blueprint, make_response, jsonify
 
-api = None
+api_view = Blueprint("api_view", __name__)
+
+@api_view.route("/test", methods=["GET"])
+def test_api():
+    return jsonify({'IT IS' : 'WORKING'}), 200
+    #return make_response(jsonify({'status' : "OK",
+    #                              'message' : "Hello this works!"})), 200
 
 def load_api(app):
     global api
-    api = Api(app)
-    print("loading")
-    NodeDefender.frontend.api.config.load_api(api)
+    NodeDefender.app.register_blueprint(api_view, url_prefix="/api/v1")
     return True
+
+import NodeDefender.frontend.api.config
--- NodeDefender/frontend/api/config.py
+++ NodeDefender/frontend/api/config.py
@@ -1,40 +1,69 @@
-import json
-from flask_restful import Resource, reqparse
+from flask import jsonify, request
 import NodeDefender
+from NodeDefender.frontend.api import api_view
 
-parser = reqparse.RequestParser()
-parser.add_argument("config")
+@api_view.route("/config/write", methods=["POST"])
+def write_config():
+    pass
 
-def load_api(api):
-    api.add_resource(GeneralConfig, '/api/v1/config/general')
-    print("loaded")
-    return True
+@api_view.route("/config/default", methods=["POST"])
+def default_config():
+    pass
 
-class General(Resource):
-    def get(self):
-        config = NodeDefender.config.general.config
-        return config
-    def put(self):
-        args = parser.parse_args()
-        print(args)
-        json.loads(args['config'].replace("'", "\""))
-        try:
-            config = json.loads(args['config'])
-        except (KeyError, ValueError):
-            return args, 400
-        print(config)
-        config = NodeDefender.config.general.set_config(**config)
-        return args, 202
+@api_view.route("/config/general", methods=["GET", "POST"])
+def config_general():
+    if request.method == "POST":
+        config = request.get_json()
+        print("Config: ", config)
+        for key, value in config.items():
+            print("KEY: {}, VALUE: {}".format(key, value))
+        #NodeDefender.config.general.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.general.config
+    return jsonify(config), 200
 
-class GeneralConfig(Resource):
-    def get(self, key):
-        try:
-            return NodeDefender.config.general.config[key]
-        except KeyError:
-            return None
+@api_view.route("/config/database", methods=["GET", "POST"])
+def config_database():
+    if request.method == "POST":
+        config = request.get_json()
+        #NodeDefender.config.database.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.database.config
+    return jsonify(config), 200
 
-    def put(self, key):
-        pass
+@api_view.route("/config/cache", methods=["GET", "POST"])
+def config_cache():
+    if request.method == "POST":
+        config = request.get_json()
+        NodeDefender.config.redis.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.redis.config
+    return jsonify(config), 200
 
+@api_view.route("/config/mail", methods=["GET", "POST"])
+def config_mail():
+    if request.method == "POST":
+        config = request.get_json()
+        NodeDefender.config.mail.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.mail.config
+    return jsonify(config), 200
 
+@api_view.route("/config/logging", methods=["GET", "POST"])
+def config_logging():
+    if request.method == "POST":
+        config = request.get_json()
+        NodeDefender.config.logging.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.logging.config
+    return jsonify(config), 200
+
+@api_view.route("/config/celery", methods=["GET", "POST"])
+def config_celery():
+    if request.method == "POST":
+        config = request.get_json()
+        NodeDefender.config.celery.set_config(**config)
+        return jsonify(config), 200
+    config = NodeDefender.config.celery.config
+    return jsonify(config), 200
 
